769e6718479d47892f53ff12bedc21bff1859b14
mthhenri <mathsilvaesilva@gmail.com>
2023-09-27 11:26:41 -0300

    Atualização das listas e configuração inicial base do DB

--- app/src/main/java/br/edu/up/app/ui/produto/ProdutosAdapter.kt
+++ app/src/main/java/br/edu/up/app/ui/produto/ProdutosAdapter.kt
@@ -3,7 +3,10 @@ package br.edu.up.app.ui.produto
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import br.edu.up.app.data.Fotos
+import br.edu.up.app.data.Produto
 import br.edu.up.app.databinding.FragmentItemProdutoBinding
 
 import br.edu.up.app.ui.produto.placeholder.PlaceholderContent.PlaceholderItem
@@ -13,7 +16,7 @@ import br.edu.up.app.ui.produto.placeholder.PlaceholderContent.PlaceholderItem
  * TODO: Replace the implementation with code for your data type.
  */
 class ProdutosAdapter(
-    private val values: List<PlaceholderItem>
+    private val produtos: List<Produto>
 ) : RecyclerView.Adapter<ProdutosAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -29,21 +32,21 @@ class ProdutosAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = values[position]
-        holder.idView.text = item.id
-        holder.contentView.text = item.content
+        val produto = produtos[position]
+            val idFoto = Fotos.get(produto.foto)
+            holder.imgFoto.setImageResource(idFoto)
+            holder.txtNomeProd.text = produto.nome
+            holder.txtPreco.text = "R$ " + String.format("%.2f", produto.preco).replace(".", ",")
+//        holder.idView.text = item.id
+//        holder.contentView.text = item.content
     }
 
-    override fun getItemCount(): Int = values.size
+    override fun getItemCount(): Int = produtos.size
 
     inner class ViewHolder(binding: FragmentItemProdutoBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        val idView: TextView = binding.itemNumber
-        val contentView: TextView = binding.content
-
-        override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
+            val imgFoto: ImageView = binding.imgFoto
+            val txtNomeProd: TextView = binding.txtNomeProd
+            val txtPreco: TextView = binding.txtPreco
         }
-    }
-
 }
